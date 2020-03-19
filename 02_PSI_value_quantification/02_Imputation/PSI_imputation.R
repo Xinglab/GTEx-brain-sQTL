de8f5fee7a6966513ce7e53660327b60a884a25d
YidaZhang0628 <yidazhang.tongji@yahoo.com>
2020-03-19 12:19:54 -0400

    Update PSI_imputation.R

--- 02_PSI_value_quantification/02_Imputation/PSI_imputation.R
+++ 02_PSI_value_quantification/02_Imputation/PSI_imputation.R
@@ -1,11 +1,11 @@
 splicetype="SE"      #type of alternative splicing, e.g., SE, A3SS, A5SS, MXE, IR
 counttype="JC"       #JCEC (junction count + exon body count) or JC (junction count only)
 
-inputpath=paste("/u/nobackup/yxing/PROJECT/yidazhan/research/rotation_project/GTEx_brain_project_V7/analysis/1_get_matrix_from_rMATS/result/",counttype,sep="")
+inputpath="path/of/the/result/from/01_Get_PSI_from_rMATS_output"
 setwd(inputpath)
 
 PSI_filter=read.table(paste(splicetype,"_",counttype,"_PSI_filter.txt",sep=""),sep="\t",header=T)
-#impute missing value (usually we don't like impute data but the imputed data is only used in PCA. We will use the original data in our GLMM model)
+
 library(impute)
 imputation=impute.knn(as.matrix(PSI_filter), k=30, maxp=dim(PSI_filter)[1])
 impute_PSI_filter <- imputation$data
@@ -13,7 +13,7 @@ impute_PSI_filter <- imputation$data
 impute_PSI_filter[which(impute_PSI_filter>1)]=1
 impute_PSI_filter[which(impute_PSI_filter<0)]=0
 
-outputpath=paste("/u/nobackup/yxing/PROJECT/yidazhan/research/rotation_project/GTEx_brain_project_V7/analysis/4.2_data_inspection_imputation/result/",counttype,sep="")
+outputpath="output/path"
 command=paste("mkdir -p ",outputpath,sep="")
 system(command)
 
