ccea02acd9e0adfaa2517ce905c2ecf8a6ff926b
Yida Zhang <yidazhang.tongji@yahoo.com>
2020-05-29 11:00:32 -0400

    Update 01_rMATS_on_BAM_file.sh

--- 01_rMATS_on_BAM_file/01_rMATS_on_BAM_file.sh
+++ 01_rMATS_on_BAM_file/01_rMATS_on_BAM_file.sh
@@ -1,10 +1,10 @@
 #make a sample file as this is required for rMATS
-echo “SRR1473936.bam” > SRR1473936.sample.txt
+echo "SRR1473936.bam" > SRR1473936.sample.txt     #SRR1473936.bam is an example BAM file for one sample
 
 #output folder for rMATS output
-outdir=“/u/home/h/harryyan/bigdata/gtex”
+outdir="/output/folder"
 
 #run rMATS
-python /u/home/s/shiehshi/rMATS-2017-3-15/rmats.py --b1 SRR1473936.sample.txt --od ${outdir} --tmp ${outdir}/.tmp \
-  --anchorLength 1 --readLength 76 --gtf /u/home/p/panyang/nobackup-yxing/references.annotations/37.chr/gencode.v26lift37.annotation.gtf \
+python /path/to/rMATS/rmats.py --b1 SRR1473936.sample.txt --od ${outdir} --tmp ${outdir}/.tmp \
+  --anchorLength 1 --readLength 76 --gtf /path/to/GTF/GTF_file_name.gtf \
   -t paired --task prep --nthread 8 --statoff
